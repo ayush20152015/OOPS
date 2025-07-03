d846a75942f9ff07158d1cd0df3ad80e025147ca
ayush20152015 <atyud2015@gmail.com>
2025-07-03 15:43:25 +0530

    updated

---
+++ copy_constructor.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class customer{
+  string name;
+  int ac;
+  int bal;
+  
+   public:
+
+ inline customer(string a,int b,int c): name(a),ac(b),bal(b){}
+ 
+ void display(){
+   cout<<name<<" "<<ac<<" "<<bal<<"\n";
+ }
+ 
+ customer(customer &b){
+    name=b.name;
+    ac=b.ac;
+    bal=b.bal;
+ }
+
+};
+
+int main(){
+  customer a1("ayush",100,1000);
+  a1.display();
+  // default copy constructor is always present
+ // customer a2=a1;
+
+  
+  // now to make a custom copy constructor
+  customer a2(a1);
+  a2.display();
+  
+  
+  return 0;      
+}
